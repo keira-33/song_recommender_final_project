3e1b50b40f506de22e1091ea29b5b82b41fbbf0e
keira-33 <kfrey@mail.endicott.edu>
2025-11-14 12:30:34 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/song_recommender_final_project/Song_recommender_final_project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.song_recommender_final_project;
+
+/**
+ *
+ * @author kfrey
+ */
+public class Song_recommender_final_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
